eb2dc621b669f2444fa970d08d8ea573eab0491f
solixzsys <solixzsystem@gmail.com>
2018-05-09 17:51:35 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpClientModule} from '@angular/common/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import {BarcodeScanner} from '@ionic-native/barcode-scanner';
+import {Toast} from '@ionic-native/toast';
+// import { NavController } from 'ionic-angular';
+import {DataservicesProvider} from '../providers/dataservices/dataservices';
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +17,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,6 +28,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
+    Toast,
+   
+    DataservicesProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,79 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import {BarcodeScanner} from '@ionic-native/barcode-scanner';
+import {Toast} from '@ionic-native/toast';
+import {ToastController} from 'ionic-angular';
+import {DataservicesProvider} from '../../providers/dataservices/dataservices';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  products: any[] = [];
+  selectedProduct: any;
+  productFound:boolean = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+   private barcodeScanner: BarcodeScanner,
+    private toast: Toast,
+    private dataservices: DataservicesProvider,
+    private toastctrl:ToastController,
+    private platform: Platform
+  ) {
 
-  }
+  
+      this.dataservices.getProducts()
+            .subscribe((response) => {
+              console.log(response)
+            })
+    }
 
+
+    presentToast() {
+      let toast = this.toastctrl.create({
+        message: 'Inside vScan',
+        duration: 3000
+      });
+      toast.present();
+    }
+
+  scan() {
+    // this.platform.ready().then(() => {
+      // this.presentToast();
+     
+      this.selectedProduct = {};
+      
+      this.barcodeScanner.scan().then((barcodeData) => {
+      this.toast.show(barcodeData.text, '5000', 'center').subscribe(
+          toast => {
+            console.log(toast);
+          }
+      );
+        this.selectedProduct = this.products.find(product => product.plu === barcodeData.text);
+        if(this.selectedProduct !== undefined) {
+          this.productFound = true;
+        } else {
+          this.productFound = false;
+          this.toast.show(`Product not found`, '5000', 'center').subscribe(
+            toast => {
+              console.log(toast);
+            }
+          );
+        }
+      }, (err) => {
+        this.toast.show(err, '5000', 'center').subscribe(
+          toast => {
+            console.log(toast);
+          }
+        );
+      // });
+  
+    
+  });
 }
+}
+
+
+
